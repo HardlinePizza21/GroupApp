8beb1e07413116039aa391dc4ad89e1378b68e9f
HardlinePizza21 <smadrido@eafit.edu.co>
2026-03-30 17:55:18 -0500

    Docker fixed

--- src/config/env.js
+++ src/config/env.js
@@ -1,12 +1,17 @@
 import dotenv from "dotenv";
 
-const envFile =
-  process.env.NODE_ENV === "production"
-    ? ".env.production"
-    : ".env";
+let envFile = ".env";
+
+if (process.env.NODE_ENV === "production") {
+  envFile = ".production.env";
+} else if (process.env.DOCKER === "true") {
+  envFile = ".docker.env";
+}
 
 dotenv.config({ path: envFile });
 
+process.env.DATABASE_URL
+console.log()
 export default {
   PORT: process.env.PORT,
   DATABASE_URL: process.env.DATABASE_URL,
