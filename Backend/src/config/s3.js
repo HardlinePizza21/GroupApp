865d3cbdbe7aaf4407a07b4515a86b021c35bb0b
HardlinePizza21 <smadrido@eafit.edu.co>
2026-04-02 19:27:35 -0500

    AWS-SDK V3

--- Backend/src/config/s3.js
+++ Backend/src/config/s3.js
@@ -1,5 +1,5 @@
-import AWS from "aws-sdk";
+import { S3Client } from "@aws-sdk/client-s3";
 
-export const s3 = new AWS.S3({
+export const s3 = new S3Client({
   region: process.env.AWS_REGION
 });
--- Backend/src/modules/files/file.service.js
+++ Backend/src/modules/files/file.service.js
@@ -1,17 +1,20 @@
 import { s3 } from "../../config/s3.js";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
 
 export const uploadToS3 = async (file) => {
-    const params = {
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Key: `uploads/${Date.now()}-${file.originalname}`,
-        Body: file.buffer,
-        ContentType: file.mimetype
-    };
+  const key = `uploads/${Date.now()}-${file.originalname}`;
 
-    const result = await s3.upload(params).promise();
+  const command = new PutObjectCommand({
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: key,
+    Body: file.buffer,
+    ContentType: file.mimetype
+  });
 
-    return {
-        url: result.Location,
-        type: file.mimetype
-    };
+  await s3.send(command);
+
+  return {
+    url: `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`,
+    type: file.mimetype
+  };
 };
