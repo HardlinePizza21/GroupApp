102c723f2c66b9d36d03e1fbfb45974b911813c9
HardlinePizza21 <smadrido@eafit.edu.co>
2026-04-03 17:42:10 -0500

    Testing signed URL's

--- Backend/src/modules/files/file.service.js
+++ Backend/src/modules/files/file.service.js
@@ -1,20 +1,33 @@
 import { s3 } from "../../config/s3.js";
-import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export const uploadToS3 = async (file) => {
   const key = `uploads/${Date.now()}-${file.originalname}`;
 
-  const command = new PutObjectCommand({
+  // 1. Subir archivo
+  const uploadCommand = new PutObjectCommand({
     Bucket: process.env.AWS_BUCKET_NAME,
     Key: key,
     Body: file.buffer,
-    ContentType: file.mimetype
+    ContentType: file.mimetype,
   });
 
-  await s3.send(command);
+  await s3.send(uploadCommand);
+
+  // 2. Generar URL firmada para lectura
+  const getCommand = new GetObjectCommand({
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: key,
+  });
+
+  const signedUrl = await getSignedUrl(s3, getCommand, {
+    expiresIn: 3600, // 1 hora
+  });
 
   return {
-    url: `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`,
-    type: file.mimetype
+    url: signedUrl,
+    key, // 🔥 importante guardar esto en DB
+    type: file.mimetype,
   };
 };
--- Backend/src/modules/messages/message.service.js
+++ Backend/src/modules/messages/message.service.js
@@ -25,7 +25,7 @@ export const createMessage = async (
         const result = await uploadToS3(file);
 
         fileData = {
-            fileUrl: result.url,
+            fileUrl: result.key,
             fileType: result.type
         };
     }
