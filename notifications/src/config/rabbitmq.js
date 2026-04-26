8cc02c147376812059c81f333d988d284d15828c
HardlinePizza21 <smadrido@eafit.edu.co>
2026-04-26 14:47:07 -0500

    All services running locally

--- notifications/src/config/rabbitmq.js
+++ notifications/src/config/rabbitmq.js
@@ -4,22 +4,22 @@ let connection = null;
 let channel = null;
 
 export const getMQChannel = async () => {
-  if (channel) return channel;
+    if (channel) return channel;
 
-  connection = await amqp.connect(process.env.RABBITMQ_URL);
-  channel = await connection.createChannel();
+    connection = await amqp.connect(process.env.RABBITMQ_URL);
+    channel = await connection.createChannel();
 
-  connection.on("close", () => {
-    console.warn("[RabbitMQ] Connection closed");
-    connection = null;
-    channel = null;
-  });
+    connection.on("close", () => {
+        console.warn("[RabbitMQ] Connection closed");
+        connection = null;
+        channel = null;
+    });
 
-  connection.on("error", (err) => {
-    console.error("[RabbitMQ] Error:", err.message);
-    connection = null;
-    channel = null;
-  });
+    connection.on("error", (err) => {
+        console.error("[RabbitMQ] Error:", err.message);
+        connection = null;
+        channel = null;
+    });
 
-  return channel;
+    return channel;
 };
